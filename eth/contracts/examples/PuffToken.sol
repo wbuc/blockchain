8ab7a3d5c7cede31efa63b05f8af7c2aa1dccbd1
Wessel Buchling <wbuchling@live.com>
2017-11-08 21:13:55 +0000

    PuffToken.sol
    Simple contract token that enables register and transfer of tokens between accounts.

---
+++ eth/contracts/examples/PuffToken.sol
@@ -0,0 +1,29 @@
+contract PuffToken {
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function PuffToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+        /* Notify anyone listening that this transfer took place */
+        Transfer(msg.sender, _to, _value);
+        
+    }
+}
